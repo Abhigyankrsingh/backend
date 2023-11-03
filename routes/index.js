59c39bc66b24ab8216b763ed09cb08fb10c37889
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-04 01:16:18 +0530

    updating the code

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const userModel = require("./users");
 
-router.get('/', function(req,res){
+router.use((req, res, next) => {
+  // Middleware logic
+  // Do operations
+  next(); // Don't forget to call next() to move to the next middleware
+  // Send response after other middlewares or route handlers are executed
+  res.send("running");
+});
+
+/* GET home page. */
+router.get('/', function(req, res) {
   res.render("index");
-})
+});
+
+router.get('/create', async function(req, res) {
+  const createdUser = await userModel.create({
+    username: "Abhi",
+    age: 21 // Assuming this should be the age value
+  });
+
+  // Send a response or handle the created user as needed
+  res.send("User created: " + createdUser);
+});
 
 module.exports = router;
