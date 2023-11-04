0e940db26d56292e145845a40dfe19c1679edaab
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-04 14:17:05 +0530

    Code Updated

--- routes/index.js
+++ routes/index.js
@@ -15,9 +15,17 @@ router.get('/create', async function(req, res) {
     age: 21 // Assuming this should be the age value
   });
 
+
   // Send a response or handle the created user as needed
   res.send("User created: " + createdUser); // Use res.send to send a string as a response
 });
+
+
+router.get("/allusers", async function(req,res){
+  let allusers = await userModel.find();
+  res.send()
+})
+
 
 
 module.exports = router;
